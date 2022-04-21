c6b4b10c29519f5dbae9b054895e9181b1f01c64
wdserverr <42107311+wdserverr@users.noreply.github.com>
2022-04-21 21:18:45 +0700

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Hero.css";
+import { useNavigate } from "react-router-dom";
+import Header from "./Header";
+
+const Hero = () => {
+  let navigate = useNavigate();
+
+  const goExplore = () => {
+    navigate("/explore");
+  };
+  const goCreate = () => {
+    navigate("/community");
+  };
+
+  return (
+    <div id="hero">
+      {/* <img id='hero-background' src={list[0].src}/> */}
+
+      <Header />
+
+      <h1 id="header-text-first"> Khong Fkers </h1>
+      <h1 id="header-text-second"> NFT Marketplace</h1>
+      <h5 id="header-subtext">Craft, hunt and trade NFT's in the dark</h5>
+
+      <div id="hero-buttons">
+        <button id="explore" onClick={goExplore}>
+          Explore
+        </button>
+        <button id="create" onClick={goCreate} disabled>Create</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
